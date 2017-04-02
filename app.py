52871368b6d9f47351fb516438f3865c5e5e7e19
jetdocument <jetdocument@gmail.com>
2017-04-02 21:24:28 +0700

    ping google

--- app.py
+++ app.py
@@ -7,13 +7,19 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
-app = Flask(__name__)
+import os
 
 @app.route("/")
 def hello():
 	line_bot_api = LineBotApi('wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=')
 	
-	line_bot_api.push_message('Ua19821cd93141008d26221f16381d256', TextSendMessage(text='From line bot Python'))
+	hostname = "google.com" #example
+	response = os.system("ping -c 1 " + hostname)
+	#and then check the response...
+	if response == 0:
+	  line_bot_api.push_message('Ua19821cd93141008d26221f16381d256', TextSendMessage(text=hostname+'Link\'s Up'))
+	else:
+	  line_bot_api.push_message('Ua19821cd93141008d26221f16381d256', TextSendMessage(text=hostname+'Link\'s Down'))	
 
     	return line_bot_api
 
