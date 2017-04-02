5d49a8ceb1f04c2a2d23da9f9891cbb683177cb3
jetdocument <jetdocument@gmail.com>
2017-04-02 21:26:47 +0700

    app flask

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from linebot.exceptions import LineBotApiError
 
 import os
 
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
 	line_bot_api = LineBotApi('wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=')
