dc996a4454f0ad007f6db917362fab44d41960f5
jetdocument <jetdocument@gmail.com>
2017-04-02 15:25:49 +0700

    api

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask
 import json
 import requests
 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	line_bot_api = LineBotApi('wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=')
+
+try:
+    line_bot_api.reply_message('<reply_token>', TextSendMessage(text='Hello World!'))
+except LineBotApiError as e:
+	
     return "OK!"
 
 @app.route('/callback', methods=['POST'])
