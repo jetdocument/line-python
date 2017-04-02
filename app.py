8af05e6d6e16d20c6347c54f55c393fbb10d8a0a
jetdocument <jetdocument@gmail.com>
2017-04-02 20:49:47 +0700

    text Changed

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ app = Flask(__name__)
 def hello():
 	line_bot_api = LineBotApi('wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=')
 	
-	line_bot_api.push_message('Ua19821cd93141008d26221f16381d256', TextSendMessage(text='Hello World!'))
-	except LineBotApiError as e:
-		pass
+	line_bot_api.push_message('Ua19821cd93141008d26221f16381d256', TextSendMessage(text='From line bot Python'))
+
     	return "OK!"
 
 @app.route('/callback', methods=['POST'])
