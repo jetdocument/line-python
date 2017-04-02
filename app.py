0ddcd5bfa7e4a31e75d6f78cc46e80699d14574f
jetdocument <jetdocument@gmail.com>
2017-04-02 15:15:51 +0700

    json

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def hello():
 
 @app.route('/callback', methods=['POST'])
 def callback():
+	json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print("ผู้ใช้：",user)
+    sendText(user,'งง')
 	return '',200
 
 if __name__ == "__main__":
