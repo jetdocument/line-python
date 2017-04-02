c8602855998d5a3e828d10567fa68c4e6fa6c72f
jetdocument <jetdocument@gmail.com>
2017-04-02 15:10:12 +0700

    import

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import json
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "READY!"
+
+@app.route('/callback', methods=['POST'])
+def callback():
+	return '',200
 
 if __name__ == "__main__":
     app.run()
