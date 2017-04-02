9f6b410fb5955cd4fb7b1b20ddafc99d8749f9aa
jetdocument <jetdocument@gmail.com>
2017-04-02 16:05:52 +0700

    notify

--- app.py
+++ app.py
@@ -1,52 +1,13 @@
-from flask import Flask
-from flask import request
-import json
-import requests
-
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "OK!"
-
-@app.route('/callback', methods=['POST'])
-def callback():
-
-	json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-
-    return '',200
-
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+import requests,json
+import urllib.parse
  
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
+LINE_ACCESS_TOKEN="dNgrSoWFrJmMwMlYuV3kH1ZwpwstHqaYCsX4fOz4Yx1"
+url = "https://notify-api.line.me/api/notify"
  
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
  
-    #print("ข้อมูล：",data)
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-    #print(r.text)
-
-if __name__ == "__main__":
-    app.run()
+message ="ทดสอบ" # ข้อความที่ต้องการส่ง
+msg = urllib.parse.urlencode({"message":message})
+LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
+session = requests.Session()
+a=session.post(url, headers=LINE_HEADERS, data=msg)
+print(a.text)
