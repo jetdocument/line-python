915494eeaeb39cbe1389f81ba3cae99474ac4de7
jetdocument <jetdocument@gmail.com>
2017-04-02 15:31:51 +0700

    import

--- app.py
+++ app.py
@@ -12,13 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	line_bot_api = LineBotApi('wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU=')
-
-try:
-    line_bot_api.reply_message('<reply_token>', TextSendMessage(text='Hello World!'))
-except LineBotApiError as e:
-	
-    return "OK!"
+	return "OK!"
 
 @app.route('/callback', methods=['POST'])
 def callback():
