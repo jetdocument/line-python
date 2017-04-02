7b0b715205c8a995849ebe782520a720bd324b40
jetdocument <jetdocument@gmail.com>
2017-04-02 16:31:21 +0700

    code for python 2.7

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import requests,json
-import urllib.parse
- 
+from urlparse import urlparse
+import urllib
+
 LINE_ACCESS_TOKEN="dNgrSoWFrJmMwMlYuV3kH1ZwpwstHqaYCsX4fOz4Yx1"
 url = "https://notify-api.line.me/api/notify"
- 
- 
-message ="ทดสอบ" # ข้อความที่ต้องการส่ง
-msg = urllib.parse.urlencode({"message":message})
+
+message ="ทดสอบ สวัสดีชาวโลก" # ข้อความที่ต้องการส่ง
+msg = urllib.urlencode(({"message":message}))
 LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
 session = requests.Session()
 a=session.post(url, headers=LINE_HEADERS, data=msg)
