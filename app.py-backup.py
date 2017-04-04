561fdb285dc1be4c55dfb226b2591140c32b5489
jetdocument <jetdocument@gmail.com>
2017-04-04 09:02:09 +0700

    New Environment

--- app.py-backup.py
+++ app.py-backup.py
@@ -11,6 +11,11 @@ from linebot.exceptions import LineBotApiError
 
 import os
 
+Channel_ID = '1508575718'
+Channel_Secret = '51d99670e2bb8acd7433e7bf75fb5416'
+Channel_Access_Token = 'wX48QUgber5AbF+6JcsrmtphuO807pZCWAjHYTEn2fnQTnbzfsxGB3bwC26Rs6fhCiyC1NFgR3ALswB00VLpTiA77FOsuWBZONuOUa++A2tVECw5fEVumyyTOYK212GhSYdkUNJZ8SREwcG45HKnIgdB04t89/1O/w1cDnyilFU='
+userId = 'Ua19821cd93141008d26221f16381d256'
+
 app = Flask(__name__)
 
 @app.route("/")
